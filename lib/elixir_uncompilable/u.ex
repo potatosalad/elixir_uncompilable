124ea5faa45beee49552f8deb72769b9332db249
Andrew Bennett <andrew.bennett@toyotaconnected.com>
2018-05-20 09:42:49 -0500

    Sleep for 1 second inside each cwd change.

--- lib/elixir_uncompilable/u.ex
+++ lib/elixir_uncompilable/u.ex
@@ -1,5 +1,5 @@
 defmodule ElixirUncompilable.U do
-  @cd File.cd!("/tmp", fn -> :io.format('~p ~s is inside ~s~n', [self(), __MODULE__, File.cwd!()]) end)
+  @cd File.cd!("/tmp", fn -> :io.format('~p ~s is inside ~s~n', [self(), __MODULE__, File.cwd!()]); Process.sleep(1000) end)
 
   def result() do
     @cd
